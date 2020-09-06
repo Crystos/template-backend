2dc8a0eadd3389522e31211fab8b8fbb4297947f
Crysto <saza.sacorp@gmail.com>
2020-09-06 22:00:50 +0500

    Babel, Jest

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _default = (a, b) => a + b;
+
+exports.default = _default;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL3NyYy9pbmRleC5qcyJdLCJuYW1lcyI6WyJhIiwiYiJdLCJtYXBwaW5ncyI6Ijs7Ozs7OztlQUFlLENBQUNBLENBQUQsRUFBSUMsQ0FBSixLQUFVRCxDQUFDLEdBQUdDLEMiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgZGVmYXVsdCAoYSwgYikgPT4gYSArIGI7XG4iXX0=
